3b19ba6cb0f4279fe0b328dee3bf1fb90e01f8a6
Aksan114 <muhammadaksan263@gmail.com>
2024-09-21 10:53:20 +0800

    .

--- config/koneksi.go
+++ config/koneksi.go
@@ -9,7 +9,7 @@ import (
 var DB  *sql.DB
 
 func GetConnection() {
-	db, _ := sql.Open("mysql", "mysql://root:rZGuemzqgBwXhPZotNrWkSMdUsfOCDRo@mysql.railway.internal:3306/pendaftaran")
+	db, _ := sql.Open("mysql", "mysql://root:rZGuemzqgBwXhPZotNrWkSMdUsfOCDRo@junction.proxy.rlwy.net:46866/pendaftaran")
 
 	if err := db.Ping(); err != nil {
 		panic(err)
